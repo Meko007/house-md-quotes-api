6e10d5d0b7df46a9ff8e0517f3ecd5cf761066c5
Okem Onyeka <jesse007onyeka@gmail.com>
2024-03-30 21:13:49 +0100

    setting up retrieval of quotes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,36 @@
-import express from 'express';
-import 'dotenv/config';
+import express from "express";
+import {
+	getRandomQuotes,
+	getRandomQuotesByCharacter,
+	getQuotesByEpisode
+} from "./utils/quotes";
+import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
 app.use(express.json());
 
-app.get('/', (_, res) => {
-    res.send('Hello World!');
+app.get("/", (_, res) => {
+	res.send("Hello World!");
+});
+
+app.get("/v1/random/:num?", async (req, res) => {
+	const { num } = req.params;
+	res.status(200).send(getRandomQuotes(Number(num) ? Number(num) : 1));
+});
+
+app.get("/v1/character/:slug/:num?", async (req, res) => {
+	const { slug, num } = req.params;
+	const choiceNum = Number(num) ? Number(num) : 1;
+	res.status(200).send(getRandomQuotesByCharacter(slug, choiceNum));
+});
+
+app.get("/v1/episode/:episode", async (req, res) => {
+	const { episode } = req.params;
+	res.status(200).send(getQuotesByEpisode(episode));
 });
 
 app.listen(port, () => {
-    console.log(`server is listening on ${port}`);
+	console.log(`server is listening on ${port}`);
 });
--- src/utils/quotes.ts
+++ src/utils/quotes.ts
@@ -1,38 +1,76 @@
-import * as jsonData from '../../data.json';
+import * as jsonData from "../../data.json";
 
 type QuoteData = {
-  characters: { [key: string]: { name: string } },
-  quotes: { quote: string, character: string }[]
+  "characters": { [key: string]: string },
+  "quotes": { "quote": string, "character": string, "episode": string }[]
 };
 
 const data: QuoteData = jsonData;
 
-const randomize = (): { quote: string; character: string } => {
-    const randomIndex = Math.floor(Math.random() * data.quotes.length);
-    return {
-        quote: data.quotes[randomIndex].quote,
-        character: data.characters[data.quotes[randomIndex].character]?.name
-    };
+interface quoteType {
+    "quote": string;
+    "character": string;
+    "episode": string;
+}
+
+const randomize = (quotes: quoteType[]): quoteType => {
+	const randomIndex = Math.floor(Math.random() * quotes.length);
+	return {
+		"quote": quotes[randomIndex].quote,
+		"character": data.characters[quotes[randomIndex].character],
+		"episode": quotes[randomIndex].episode
+	};
 };
 
-const getRandomQuotes = (numOfQuotes: number): { quote: string; character: string }[] => {
-    const out: { quote: string; character: string }[] = [];
-    const quotesLen = data.quotes.length;
+export const getRandomQuotes = (numOfQuotes: number): quoteType[] => {
+	const out: quoteType[] = [];
+	const quotesLen = data.quotes.length;
 
-    if (numOfQuotes > quotesLen) {
-        numOfQuotes = quotesLen;
-    }
+	if (numOfQuotes > quotesLen) {
+		numOfQuotes = quotesLen;
+	}
 
-    while (out.length < numOfQuotes) {
-        const randomQuote = randomize();
-        const existingQuote = out.find(quote => quote.quote === randomQuote.quote);
-        if (!existingQuote) {
-            out.push(randomQuote);
-        }
-    }
+	while (out.length < numOfQuotes) {
+		const randomQuote = randomize(data.quotes);
+		const existingQuote = out.find(quote => quote.quote === randomQuote.quote);
+		if (!existingQuote) {
+			out.push(randomQuote);
+		}
+	}
 
-    return out;
+	return out;
 };
 
-console.log(getRandomQuotes(20));
+export const getRandomQuotesByCharacter = (slug: string, numOfQuotes: number): quoteType[] => {
+	const characterQuotes = data["quotes"].filter(quote => quote.character === slug);
 
+	if (characterQuotes.length === 0) {
+		return [];
+	}
+
+	const out: quoteType[] = [];
+
+	if (numOfQuotes > characterQuotes.length) {
+		numOfQuotes = characterQuotes.length;
+	}
+
+	while (out.length < numOfQuotes) {
+		const randomQuote = randomize(characterQuotes);
+		const existingQuote = out.find(quote => quote.quote === randomQuote.quote);
+		if (!existingQuote) {
+			out.push(randomQuote);
+		}
+	}
+
+	return out;
+};
+
+export const getQuotesByEpisode = (episode: string): quoteType[] => {
+	const filteredQuotes = data["quotes"].filter(quote => quote.episode === episode);
+
+	for (const quote of filteredQuotes) {
+		quote.character = data.characters[quote.character];
+	}
+
+	return filteredQuotes;
+};
